a58c66bb2ed18c074d26dd878e48ebead342fd73
heqiang <15261813546@163.xom>
2019-01-09 21:06:59 +0800

    ddd

--- php/sample.php
+++ php/sample.php
@@ -44,9 +44,10 @@ $signPackage = $jssdk->GetSignPackage();
 	  wx.ready(function () {
 	    // 在这里调用 API
 	    wx.updateAppMessageShareData({ 
-	        title: '测试', // 分享标题
-	        desc: '测试', // 分享描述
-	        link: 'http://8.9.6.220/template/php/simple.php', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+	        title: '开开开心', // 分享标题
+	        desc: '哈哈哈哈，就是开心！！！', // 分享描述
+	        // link: 'http://8.9.6.220/template/php/simple.php', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+	        link:window.location.href;
 	        imgUrl: '', // 分享图标
 	        success: function () {
 	          alert('updateAppMessageShareData success ');
